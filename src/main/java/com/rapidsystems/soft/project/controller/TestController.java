bebea2859f7dcb23f1867b536dcc84d9a1f07d3a
Valeriy Kashin <valera-519@mail.ru>
2022-11-21 19:20:52 +0300

    Refactoring

--- src/main/java/com/rapidsystems/soft/project/controller/TestController.java
+++ src/main/java/com/rapidsystems/soft/project/controller/TestController.java
@@ -3,5 +3,5 @@ package com.rapidsystems.soft.project.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyTestSpringJavaControllerCrmRapidSystemController {
+public class TestController {
 }
