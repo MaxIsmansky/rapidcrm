4489426a17b2469fa2a8494ff99acd5c6556ab5a
Valeriy Kashin <valera-519@mail.ru>
2022-11-21 19:14:58 +0300

    Create true controller

---
+++ src/main/java/com/rapidsystems/soft/project/controller/MyTestSpringJavaControllerCrmRapidSystemController.java
@@ -0,0 +1,7 @@
+package com.rapidsystems.soft.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyTestSpringJavaControllerCrmRapidSystemController {
+}
